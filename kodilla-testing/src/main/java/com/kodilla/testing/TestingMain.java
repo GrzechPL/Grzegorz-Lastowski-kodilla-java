21147d16fed28a51be42f8df5bc6939cffe20fdb
“GrzechPL <grzech78@o2.pl>
2017-09-07 13:27:24 +0200

    “modul

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        int arg1 = 50;
+        int arg2 = 30;
+        Calculator calculator = new Calculator(arg1,arg2);
+        int resultadd = calculator.addAB();
+        if(resultadd==arg1+arg2){
+            System.out.println("Test add ok");
+        } else {
+            System.out.println("Error add");
+        }
+        int resultsub = calculator.subAB();
+        if(resultsub==arg1-arg2){
+            System.out.println("Test sub ok");
+        } else {
+            System.out.println("Error sub");
+        }
+        }
     }
-}
