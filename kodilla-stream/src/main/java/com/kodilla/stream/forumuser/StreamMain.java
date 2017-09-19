f982d82342133dcc67c257e3166dacc7ca94b949
“GrzechPL <grzech78@o2.pl>
2017-09-20 00:35:32 +0200

    20/09

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
@@ -1,14 +1,29 @@
 package com.kodilla.stream.forumuser;
 
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StreamMain {
     public static void main(String[] args) {
+
         Forum forum = new Forum();
         //inicjalizacja stream
-        forum.getUserList().stream()
-                //operacje filtrujące
+            //zmiennej userMap o typie Map przypisujemy stream pobierany z metody getUserList()
+        Map<Integer,ForumUser> userMap = forum.getUserList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> forumUser.getDateOfBirth().getYear() < 1997 )
+                .filter(forumUser -> forumUser.getDateOfBirth().isBefore(LocalDate.now().minusYears(20)))
                 .filter(forumUser -> forumUser.getPostCounter() > 0)
+                //kolektory tworzą kolekcje wynikowe
+                //wynikiem tego ma być kolekcja typu map
+                //uzyta jest referencja klasy :: do metody, oraz funkcja
+                //kluczem ma być metoda getUserId
+                //wartością mapy ma być obiekt forum user
+                .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
+        //drukowanie na konsoli
+        System.out.println("# elements: " + userMap.size());
+        userMap.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
 
     }
