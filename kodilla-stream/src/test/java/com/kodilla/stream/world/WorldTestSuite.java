0da5d0dd4eac0e3884b6249d4b3511495cb6a35f
“GrzechPL <grzech78@o2.pl>
2017-09-22 12:22:13 +0200

    22/09

--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -3,14 +3,6 @@ import org.junit.*;
 import java.math.BigDecimal;
 
 public class WorldTestSuite {
-    @Before
-    public void before(){
-        System.out.println("Test Case: begin");
-    }
-    @After
-    public void after(){
-        System.out.println("Test Case: end");
-    }
     @BeforeClass
     public static void beforeClass() {
         System.out.println("Test Suite: begin");
@@ -22,29 +14,37 @@ public class WorldTestSuite {
     @Test
     public void testGetPeopleQuantity(){
         //Given
-        World world = new World();
-        world.addContinent("Europa");
-        world.addContinent("Ameryka");
-        world.addContinent("Afryka");
+        //tworze obiekt klasy Country z dwoma polami nameOfCountry,numberOfPeople
+        Country pl =new Country("Polska", new BigDecimal("100000000000000020"));
+        Country de =new Country("Niemcy", new BigDecimal("100000000000000020"));
+        Country fr =new Country("Francja", new BigDecimal("100000000000000030"));
+        Country st =new Country("Stany Zjednoczone", new BigDecimal("100000000000000040"));
+        Country ko =new Country("Kongo", new BigDecimal("100000000000000050"));
+        Country ni =new Country("Nigeria", new BigDecimal("100000000000000060"));
 
-        Continent continent = new Continent();
-        continent.addCountry("Polska");
-        continent.addCountry("Niemcy");
-        continent.addCountry("USA");
-        continent.addCountry("Kamerun");
-        continent.addCountry("Francja");
+        //tworze obiekty klasy Continent z jednym polem nameOfContinent
+        Continent europa = new Continent("Europa");
+        Continent ameryka = new Continent("Ameryka");
+        Continent afryka = new Continent("Afryka");
 
-        Country country1 =new Country("Polska", new BigDecimal("100000000000000010"));
-        Country country2 =new Country("Niemcy", new BigDecimal("100000000000000020"));
-        Country country3 =new Country("Francja", new BigDecimal("100000000000000030"));
-        Country country4 =new Country("USA", new BigDecimal("100000000000000040"));
-        Country country5 =new Country("Kongo", new BigDecimal("100000000000000050"));
-        Country country6 =new Country("Nigeria", new BigDecimal("100000000000000060"));
+        //korzystam z metody addCountry klasy Continent
+        //do obiektu europa klasy Continent dodaje obiekt klasy Country
+        europa.addCountry(pl);
+        europa.addCountry(de);
+        europa.addCountry(fr);
+        ameryka.addCountry(st);
+        afryka.addCountry(ko);
+        afryka.addCountry(ni);
 
+        //tworze obiekt klasy World oraz dodaje obiekt klasy Continent
+        World continent = new World();
+        continent.addContinent(europa);
+        continent.addContinent(ameryka);
+        continent.addContinent(afryka);
         //When
-        BigDecimal result = world.getPeopleQuantity();
+        BigDecimal result = continent.getPeopleQuantity();
         //Then
-        Assert.assertEquals(new BigDecimal ("600000000000000210"), result);
+        Assert.assertEquals(new BigDecimal ("600000000000000220"), result);
     }
 
 }
