639ba43a8831b99bd5169b52d3185677b4bfc78e
“GrzechPL <grzech78@o2.pl>
2017-10-03 16:46:25 +0200

    03/10

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindFlight {
+
+    public void findFilght(Flight flight) {
+
+        Map<String, Boolean> hashMap = new HashMap<>();
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+
+
