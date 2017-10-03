ddb9e9e832adec3f3a879b7b2194b588bff4b7e5
“GrzechPL <grzech78@o2.pl>
2017-10-03 13:46:36 +0200

    03/10

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,19 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(1.5git,2);
+
+        }catch (Exception e){
+            System.out.println("Zły wpis");
+        }
+        finally {
+            System.out.println("Nie usuniesz mnie nigdy HAHAHA");
+        }
+    }
+}
