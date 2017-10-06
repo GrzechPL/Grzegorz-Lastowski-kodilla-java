e1b23bd9276b3c28a25622e258312ef9cc852171
“GrzechPL <grzech78@o2.pl>
2017-10-06 18:39:49 +0200

    06/10

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flycompany/MainFly.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flycompany/MainFly.java
@@ -25,6 +25,7 @@ public class MainFly {
             .filter(x->city.equals(x.flightFROM))
             .collect(Collectors.toList());
 
+        System.out.println("Wszystkie połączenia z " + city);
       for(Flight temp :formcity){
           System.out.println(temp.flightFROM + "-->>" + temp.flightTO);
     }
@@ -35,29 +36,27 @@ public class MainFly {
         List<Flight> formcity =allFly().stream()
                 .filter(x->city.equals(x.flightTO))
                 .collect(Collectors.toList());
-
+        System.out.println("Wszystkie połączenia do " + city);
         for(Flight temp :formcity){
-            System.out.println(temp.flightTO + "-->>" + temp.flightFROM);
+            System.out.println(temp.flightFROM + "-->>" + temp.flightTO);
         }
     }
 
-    public void findThrowAnotherCity(String city1,String city2){
+    public void findThrowAnotherCity(String city1,String city2,String citythrow){
 
         List<Flight> formcity1 =allFly().stream()
+                .filter(x->citythrow.equals(x.flightTO))
                 .filter(x->city1.equals(x.flightFROM))
-                .filter(x->!city2.equals(x.flightTO))
                 .collect(Collectors.toList());
-
+        System.out.println("Połączenie "+ city1+" do "+city2 +" z przesiadką w " +citythrow );
         for(Flight temp :formcity1){
             System.out.println(temp.flightFROM + "-->>" + temp.flightTO);
         }
 
         List<Flight> formcity2 =allFly().stream()
+                .filter(x->citythrow.equals(x.flightFROM))
                 .filter(x->city2.equals(x.flightTO))
-               //.filter(x->formcity1.contains(x.flightTO))
                 .collect(Collectors.toList());
-
-        System.out.println();
         for(Flight temp :formcity2){
             System.out.println(temp.flightFROM + "-->>" + temp.flightTO);
         }
@@ -67,9 +66,9 @@ public class MainFly {
     public static void main(String[] args) {
         MainFly mainFly = new MainFly();
         mainFly.findFromCity("Wrocław");
-       System.out.println();
+        System.out.println();
         mainFly.findToCity("Warszawa");
         System.out.println();
-        mainFly.findThrowAnotherCity("Wrocław","Warszawa");
+        mainFly.findThrowAnotherCity("Wrocław","Warszawa","Poznań");
     }
 }
