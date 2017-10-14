b7c9cc3cc6812fa068b3ee98633d2d0ad72b0e98
“GrzechPL <grzech78@o2.pl>
2017-10-14 21:22:00 +0200

    14/10

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,25 +1,37 @@
 package com.kodilla.spring.portfolio;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class BoardConfig {
 
         @Bean
         public TaskList getToDoList(){
-        return new TaskList(new ArrayList<>());
+
+        List<String> task1 = new ArrayList();
+        task1.add("Zadanie 1");
+
+        return new TaskList(task1);
         }
         @Bean
         public TaskList getInProgressList(){
-                return new TaskList(new ArrayList<>());
+
+                List<String> task2 = new ArrayList();
+                task2.add("Zadanie 2");
+
+                return new TaskList(task2);
         }
         @Bean
         public TaskList getDoneList(){
-                return new TaskList(new ArrayList<>());
+
+                List<String> task3 = new ArrayList();
+                task3.add("Zadanie 3");
+
+                return new TaskList(task3);
         }
         //następuje wstrzyknięcie trzech obiektów do konstruktora klasy Bean
         @Bean
--- kodilla-spring/src/test/java/com/kodilla/spring/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/BoardTestSuite.java
@@ -17,25 +17,14 @@ public class BoardTestSuite {
     @Test
     public void testTaskAdd() {
         //Given
-        List<String> task1 = new LinkedList<>();
-        task1.add("Zadanie 1");
-        List<String> task2 = new LinkedList<>();
-        task2.add("Zadanie 2");
-        List<String> task3 = new LinkedList<>();
-        task3.add("Zadanie 3");
 
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
-
         Board board = context.getBean(Board.class);
-
-        board.getToDoList().setTasks(task1);
-        board.getDoneList().setTasks(task2);
-        board.getInProgressList().setTasks(task3);
 
         //When
         String restult1 = board.getToDoList().getTasks().get(0);
-        String restult2 = board.getDoneList().getTasks().get(0);
-        String restult3 = board.getInProgressList().getTasks().get(0);
+        String restult2 = board.getInProgressList().getTasks().get(0);
+        String restult3 = board.getDoneList().getTasks().get(0);
 
         //Then
         Assert.assertEquals("Zadanie 1", restult1);
