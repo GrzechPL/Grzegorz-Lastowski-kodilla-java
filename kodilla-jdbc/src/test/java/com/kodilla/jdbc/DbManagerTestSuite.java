ecbace55548b5a83cc0d3a5a9ba2841315461c97
“GrzechPL <grzech78@o2.pl>
2017-12-18 18:49:37 +0100

    18/12/17

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -16,56 +16,57 @@ public class DbManagerTestSuite {
         //Then
         Assert.assertNotNull(dbManager.getConnection());
     }
-
-    @Test
-    public void testSelectUsers() throws SQLException {
-        //Given
-        DbManager dbManager = DbManager.getInstance();
-
-        //When
-        String sqlQuery = "SELECT * FROM USERS";
-        Statement statement = dbManager.getConnection().createStatement();
-        ResultSet rs = statement.executeQuery(sqlQuery);
-
-        //Then
-        int counter = 0;
-        while(rs.next()) {
-            System.out.println(rs.getInt("ID") + ", " +
-                    rs.getString("FIRSTNAME") + ", " +
-                    rs.getString("LASTNAME"));
-            counter++;
-        }
-        rs.close();
-        statement.close();
-        Assert.assertEquals(5, counter);
-    }
-
-    @Test
-    public void testSelectUsersAndPosts() throws SQLException {
-        //Given
-        DbManager dbManager = DbManager.getInstance();
-
-        //When
-        String sqlQuery = "SELECT U.FIRSTNAME,U.LASTNAME,COUNT(*) AS TASK_NUMBER\n" +
-                "FROM TASK T, USERS U\n" +
-                "WHERE U.ID = T.USER_ID_ASSIGNEDTO\n" +
-                "GROUP BY T.USER_ID_ASSIGNEDTO\n" +
-                "HAVING COUNT(*) > 1;";
-        Statement statement = dbManager.getConnection().createStatement();
-        ResultSet rs = statement.executeQuery(sqlQuery);
-
-        //Then
-        int counter = 0;
-        while(rs.next()) {
-            System.out.println( rs.getString("FIRSTNAME")
-                    + " "
-                    + rs.getString("LASTNAME")
-                    + "\t"
-                    + rs.getInt("TASK_NUMBER"));
-            counter++;
-        }
-        rs.close();
-        statement.close();
-        Assert.assertEquals(4, counter);
-    }
 }
+
+   // @Test
+   // public void testSelectUsers() throws SQLException {
+        //Given
+    //    DbManager dbManager = DbManager.getInstance();
+
+        //When
+    //    String sqlQuery = "SELECT * FROM USERS";
+    //    Statement statement = dbManager.getConnection().createStatement();
+    //    ResultSet rs = statement.executeQuery(sqlQuery);
+
+     //   //Then
+     //   int counter = 0;
+     //   while(rs.next()) {
+     //       System.out.println(rs.getInt("ID") + ", " +
+     //               rs.getString("FIRSTNAME") + ", " +
+     //               rs.getString("LASTNAME"));
+     //       counter++;
+     //   }
+     //   rs.close();
+     //   statement.close();
+     //   Assert.assertEquals(5, counter);
+   // }
+
+ //   @Test
+ //   public void testSelectUsersAndPosts() throws SQLException {
+ //       //Given
+ //       DbManager dbManager = DbManager.getInstance();
+//
+        //When
+ //       String sqlQuery = "SELECT U.FIRSTNAME,U.LASTNAME,COUNT(*) AS TASK_NUMBER\n" +
+ //               "FROM TASK T, USERS U\n" +
+ //               "WHERE U.ID = T.USER_ID_ASSIGNEDTO\n" +
+ //               "GROUP BY T.USER_ID_ASSIGNEDTO\n" +
+ //               "HAVING COUNT(*) > 1;";
+ //       Statement statement = dbManager.getConnection().createStatement();
+ //       ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+ //       int counter = 0;
+ //       while(rs.next()) {
+ //           System.out.println( rs.getString("FIRSTNAME")
+ //                   + " "
+ //                   + rs.getString("LASTNAME")
+ //                   + "\t"
+ //                   + rs.getInt("TASK_NUMBER"));
+ //           counter++;
+  //      }
+  //      rs.close();
+  //      statement.close();
+  //      Assert.assertEquals(4, counter);
+  //  }
+//}
