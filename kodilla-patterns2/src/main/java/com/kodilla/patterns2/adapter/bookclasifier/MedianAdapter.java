d5602867267d7a730d31fd32e8c314e4c95b2ccb
“GrzechPL <grzech78@o2.pl>
2017-12-21 16:25:55 +0100

    21/12/17

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -4,12 +4,24 @@ import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
 import com.kodilla.patterns2.adapter.bookclasifier.librarya.Classifier;
 
 
+import java.util.Arrays;
 import java.util.Set;
 
 public class MedianAdapter extends MedianAdaptee implements Classifier{
     @Override
     public int publicationYearMedian(Set<Book> bookSet) {
-
-        return 0;
+        if (bookSet.size() == 0) return 0;
+        int [] years = new int[bookSet.size()];
+        int n = 0;
+        for (Set.<Book> entry :bookSet){
+            years[n] = entry.getYearOfPublication();
+            n++;
+        }
+        Arrays.sort(years);
+        if(years.length % 2 == 0){
+            return years[(int)(years.length/2+0.5)];
+        }else{
+            return years[years.length/2];
+        }
         }
 }
